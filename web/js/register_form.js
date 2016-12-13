8cea1c6e6845e63d527e7e13961c3fd7c90922ac
MaryTory <m.ignatovska@mail.ru>
2016-12-13 19:58:10 +0200

    Рабочие счетчики, запоминание где был пользователь, изменение цвета посещенной обл

--- web/js/register_form.js
+++ web/js/register_form.js
@@ -4,7 +4,7 @@ $(document).ready(
                $("#register :input").serializeArray(),
         function(info){
             if (info == 'ok') {
-                $('#btnCloseRegister').click()
+                window.location.href = "profile.php";
             };
             $("#ack").empty();
             $("#ack").html(info);
